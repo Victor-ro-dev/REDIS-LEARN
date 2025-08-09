b5ea2b830431e73c6e8ced09d6f474b692be0d77
Victor-ro-dev <victor.ro.dev10@gmail.com>
2025-08-09 20:50:40 -0300

    Adiciona métodos para inserção com expiração no repositório Redis e atualiza o script de execução para utilizar esses métodos.

--- models/redis_repository.py
+++ models/redis_repository.py
@@ -17,3 +17,10 @@ class RedisRepository:
     def get_hash(self, hash_name: str, field: str) -> str:
         value = self.__redis_connection.hget(hash_name, field)
         return value.decode('utf-8') if value else None
+
+    def insert_ex(self, key: str, value: str, ex: int) -> None:
+        self.__redis_connection.set(key, value, ex=ex)
+
+    def insert_hash_ex(self, hash_name: str, field: str, value: str, ex: int) -> None:
+        self.__redis_connection.hset(hash_name, field, value)
+        self.__redis_connection.expire(hash_name, ex)
--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 from models.connection.redis_connection import RedisConnectionHandle
 from models.redis_repository import RedisRepository
 
+from datetime import datetime
 
 redis_handle = RedisConnectionHandle()
 redis_conn = redis_handle.connect()
 repository = RedisRepository(redis_conn)
-repository.insert('user:name', 'John Doe')
-repository.insert('user:email', 'john.doe@example.com')
 
 
+current_data = datetime.now().strftime("%Y-%m-%d")
+
+print(current_data)
+
+repository.insert_hash_ex(current_data, 'banana', 3.12, 40)
+repository.insert_hash(current_data, 'apple', 2.34)
+repository.insert_hash(current_data, 'uva', 7.34)
